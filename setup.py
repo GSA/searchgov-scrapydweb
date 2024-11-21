703fa912a368747e2491416b07cee88f03aea0c4
IsabelLaurenceau <isabel.laurenceau@gsa.gov>
2024-11-21 13:35:30 -0500

    upgrade

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ setup(
     install_requires=[
         "APScheduler==3.10.0",  # Mar 18, 2019
         "colorama==0.4.0",  # Oct 10, 2018
-        "Flask==2.2.5",  # Nov 19, 2024
+        "Flask==3.1.0",  # Nov 19, 2024
         "Flask-Compress==1.4.0",  # Jan 5, 2017
-        "Flask-SQLAlchemy==3.0.2",  # Nov 21, 2024
+        "Flask-SQLAlchemy==3.1.1",  # Nov 21, 2024
         "logparser==0.8.2",
         "requests>=2.21.0",  # Dec 10, 2018
     ],
